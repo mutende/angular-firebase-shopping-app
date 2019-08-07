9dde8310228f4089837f5fcc4ca8368a71278f6e
Elvis Mutende <elvismutende@gmail.com>
2019-08-07 11:56:55 +0300

    adding to cart

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/shared/product.service';
 import { AuthService } from 'src/app/shared/auth.service';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-product-list',
@@ -11,8 +12,18 @@ export class ProductListComponent implements OnInit {
   productList: any[];
   rowIndexArray: any[];
   user: firebase.User;
+
+  product_name_cart: any[];
+  product_price_cart: any[];
+  product_name : any;
+  product_price : any ;
   
-  constructor(private service: ProductService,   private auth: AuthService) { }
+  cart : FormGroup;
+  constructor(private service: ProductService,   
+    private auth: AuthService, 
+    private fb: FormBuilder) {
+      this.createForm()
+     }
 
   ngOnInit() {
     this.service.productDetailList.snapshotChanges().subscribe(
@@ -26,7 +37,42 @@ export class ProductListComponent implements OnInit {
     this.auth.getuserState().subscribe(user =>{
       this.user = user;      
     });
+
+    this.product_name_cart = [
+      {}
+    ]
+    this.product_price_cart =[
+      {}
+    ]
   
+  }
+
+  createForm() {
+    this.cart = this.fb.group({
+      p_name: [''],
+      p_price: ['']
+    });
+  }
+  // handle selling
+  isBought: boolean;
+
+  //  
+  //buy function
+
+  createCart(p_name, p_price){
+    this.product_name = p_name;
+    this.product_price = p_price;
+    this.product_name_cart.push({name : this.product_name});
+    this.product_price_cart.push({name : this.product_price});
+   
+    console.log(this.product_name_cart);
+    console.log(this.product_price_cart);
+  }
+
+  buyProduct(){
+    this.isBought = true;
+
+    //clear the arrays
   }
 
 }

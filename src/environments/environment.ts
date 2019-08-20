183f423a620f4755a4d58f8f70e05a371c69bf78
Elvis Mutende <elvismutende@gmail.com>
2019-08-20 10:08:22 +0300

    hide API key info

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: 'AIzaSyCSSR0EPv5_B4bf20YIMDVZPa8awDp09EA',
+    apiKey: '<API_KEY>',
     authDomain: 'sibasi-shopping-4045d.firebaseapp.com',
     databaseURL: 'https://sibasi-shopping-4045d.firebaseio.com',
     projectId: 'sibasi-shopping-4045d',
     storageBucket: 'sibasi-shopping-4045d.appspot.com',
-    messagingSenderId: '971801271152',
-    appId: '1:971801271152:web:b49248e0b5c8d4a1'
+    messagingSenderId: '<Messaging_ID>',
+    appId: '<APP_ID>'
   }
 };
 
